ad783de1aa4694a4111867a21248680e4de0a619
Sora1937 <frostbite1937@outlook.com>
2024-07-05 10:32:29 -0700

    edited an r file
    added comments

--- RChallenge8.r
+++ RChallenge8.r
@@ -1,16 +1,23 @@
+# Set a random seed for reproducibility
 set.seed(123)
 
+# Geneerate random indices for training set
 train_indicies <- sample(1:nrow(iris), 0.7 * nrow(iris))
 
+# Create the training set
 train_data <- iris[train_indicies, ]
 
+# Create the testing set
 test_data <- iris[-train_indicies, ]
 
 library(class)
 
+# Train a k-NN classifier
 model <- knn(train = train_data[, -5], test = test_data[, -5], cl = train_data[, 5],
 k = 3)
 
+# Calculate the accuracy
 accuracy <- sum(model == test_data[, 5]) / nrow(test_data)
 
+# Print the accuracy
 cat("The accuracy of the k-NN classifier on the testing set is:", accuracy)
